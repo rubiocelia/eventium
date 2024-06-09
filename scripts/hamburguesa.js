4bea0507ede0192409900d5789b2e892c2ff4b6f
DavidOS10 <onatesanchezdavid@gmail.com>
2024-06-09 18:21:10 +0200

    comentarios

--- scripts/hamburguesa.js
+++ scripts/hamburguesa.js
@@ -1,3 +1,4 @@
+//Funcion para que la hamburguesa despliegue el menu cuando esta responsive
 document.querySelector('.hamburger').addEventListener('click', function () {
     const menu = document.querySelector('.menu');
     this.classList.toggle('active');
